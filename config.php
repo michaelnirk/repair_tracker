6c9489b4303ae61baadca4140a8899ff8296077e
Michael Nirk <nirkules@gmail.com>
2020-03-22 14:20:00 +0100

    Edited config file to reflect renaming of develope folder to develop

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
     define('PATH_SEP', ';');
-    set_include_path(get_include_path() . PATH_SEP . 'C:/develope/repair_tracker/');
-    define('SMARTY_HOME', 'C:/develope/repair_tracker/');
+    set_include_path(get_include_path() . PATH_SEP . 'C:/develop/repair_tracker/');
+    define('SMARTY_HOME', 'C:/develop/repair_tracker/');
     define('CLIENT_ID', "807899027268-16aee4oog4ij86nei402rrdupfq0fr6t.apps.googleusercontent.com");
     define('CLIENT_SECRET', 'pxPv4cG7vzrILbwKcmOoFSwr');
     define('REFRESH_TOKEN', '1/s2xNW5fGWb-DGO82Bejd1EerJ40EuyUhMwMEHfJIgRI');
