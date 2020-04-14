5de3f050487d3eb3fa8f07a5036b28e1b28d2193
Michael Nirk <nirkules@gmail.com>
2020-04-14 16:02:57 +0200

    Clean up

--- htdocs/js/session.js
+++ htdocs/js/session.js
@@ -25,19 +25,6 @@ Session = (function() {
     if (sessionStorage.getItem(key)) {
       return JSON.parse(sessionStorage.getItem(key));
     }
-//    $.ajax({
-//      url: elements[key].url,
-//      data: elements[key].data
-//    }).done(result => {
-//      if (typeof result === 'string') {
-//        sessionStorage.setItem(key, result);
-//        return JSON.parse(sessionStorage.getItem(key));
-//      } else {
-//        console.error(`Session.get(${key}) failed: ${result}`);
-//      }
-//    }).fail((xhr, statusText, error) => {
-//      console.error(`Session.get(${key}) ajax failed: status text: ${statusText}\nerror: ${error}`);
-//    });
 
     try {
       const result = await $.ajax({
